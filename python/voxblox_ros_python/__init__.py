3a2640ca083664e277b1e3a4de581ce9fc3ae9a8
HiroIshida <spitfire.docomo@gmail.com>
2021-04-09 14:51:15 +0900

    fixbug : covert to list

--- python/voxblox_ros_python/__init__.py
+++ python/voxblox_ros_python/__init__.py
@@ -1,4 +1,5 @@
 import io
+import numpy as np
 from . import _voxblox_ros_python 
 
 class EsdfMapClientInterface(object):
@@ -14,6 +15,10 @@ class EsdfMapClientInterface(object):
     def _update(self, msg_bindata):
         _voxblox_ros_python.update_esdf_map(self._ptr, msg_bindata)
 
-    def get_distance(self, pts):
+    def get_distance(self, pts_):
+        if type(pts_) == np.ndarray:
+            pts = pts_.tolist()
+        else:
+            pts = pts_
         dists = _voxblox_ros_python.get_batch_dist(self._ptr, pts)
         return dists
