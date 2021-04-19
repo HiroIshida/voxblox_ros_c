e5d1a6115df772e012fd3d15222be1527cc2fde7
HiroIshida <spitfire.docomo@gmail.com>
2021-04-19 14:58:00 +0900

    Save serialized msg also

--- data/save_pickle_msg.py
+++ data/save_pickle_msg.py
@@ -6,22 +6,22 @@ from voxblox_msgs.msg import Layer, Block
 data = {"data_sequence": [], "counter": 0}
 
 def callback(msg):
-    print("rec")
-    filename = "layer{0}.binmsg".format(data["counter"])
-
+    print("rec {0}".format(data["counter"]))
+    filename_serialized = "layer{0}.sermsg".format(data["counter"])
     s = io.BytesIO()
     msg.serialize(s)
     bindata = s.getvalue()
-    with open(filename, "w") as f:
+    with open(filename_serialized, "w") as f:
         f.write(bindata)
+
+    filename_deserialized = "layer{0}.pickle".format(data["counter"])
+    with open(filename_deserialized, "wb") as f:
+        pickle.dump(msg, f)
+
     data["counter"] += 1
 
 topic_name = "/voxblox_node/esdf_map_out"
 rospy.init_node('listener', anonymous=True)
 rospy.Subscriber(topic_name, Layer, callback)
 rospy.spin()
-
-filename = "layer_msg_seq.pickle"
-with open(filename, "wb") as f:
-    pickle.dump(data["data_sequence"], f)
 
