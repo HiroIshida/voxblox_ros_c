4239c6b2bde4c73cef3d47f0651e48a672553db6
HiroIshida <spitfire.docomo@gmail.com>
2021-04-19 13:35:41 +0900

    Build basic.c

--- test/basic.c
+++ test/basic.c
@@ -8,4 +8,8 @@ int main(){
     fread(buffer,sizeof(buffer),1,ptr);
     void* map = C_create_esdf_map(0.2, 16);
     C_update_esdf_map(map, buffer);
+    double point[3] = {1.5, 0.0, 0.8};
+    double dist;
+    C_get_dist(map, point, &dist);
+    printf("%f", dist);
 }
